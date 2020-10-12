9e5adeb443fd4b9a1fb7d503675bc29a4f5716cd
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2020-10-12 20:57:30 +0000

    criando e estilizando a OrphanageMap page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,32 +1,12 @@
 import React from "react";
-import { FiArrowRight } from "react-icons/fi";
 
 import "./styles/global.css";
-import "./styles/page/landing.css";
 
-import logoImg from "./images/logo.svg";
+import Routes from "./routes";
 
 function App() {
   return (
-    <div id="landing-page">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Happy" />
-
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças</p>
-        </main>
-
-        <div className="location">
-          <strong>Canguaretama</strong>
-          <span>Rio Grande do Norte</span>
-        </div>
-
-        <a href="#" className="enter-app">
-					<FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
-				</a>
-      </div>
-    </div>
+    <Routes />
   );
 }
 
